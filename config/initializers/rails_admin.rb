eb919b781b9697d462279cf89f6155ee349565bb
Carl Kritzinger <carl@fireid.com>
2016-09-16 09:28:23 +0200

    Always allow admin in dev mode

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -6,7 +6,7 @@ RailsAdmin.config do |config|
   config.label_methods += [:label, :name, :description, :email, :file_name]
 
   def is_admin?(user)
-    #return true if Rails.env.development?
+    return true if Rails.env.development?
     user && Settings.rails_admin.admin_white_list.split(',').include?(user.email)
   end
 
